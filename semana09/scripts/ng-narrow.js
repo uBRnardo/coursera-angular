40649f39f9550be523511a3cfef35640b46bf130
Bernardo Quaresma <bernardo@tecgraf.puc-rio.br>
2017-06-05 10:20:09 -0300

    substituição de isEmpty por if na diretiva

--- semana09/scripts/ng-narrow.js
+++ semana09/scripts/ng-narrow.js
@@ -12,11 +12,7 @@ function FoundItemsDirective() {
     scope: {
       foundItems: '<',
       onRemove: '&',
-      isEmpty: '&'
     }
-    // ,controller: NarrowItDownAppController,
-    // controllerAs: 'narrowapp',
-    // bindToController: true
   };
 
   return ddo;
@@ -28,9 +24,9 @@ function NarrowItDownAppController(MenuSearchService) { var _this = this;
 
 	_this.foundItems;
 
-	_this.narrow = function() {                                                   /*DEBUG*/ console.log('narrowing for:',_this.searchTerm);
+	_this.narrow = function() {                                                   //*DEBUG*/ console.log('narrowing for:',_this.searchTerm);
 		MenuSearchService.getMatchedMenuItems(_this.searchTerm)
-		.then(function(result) {                                                 /*DEBUG*/ console.log('got response:',result);
+		.then(function(result) {                                                 //*DEBUG*/ console.log('got response:',result);
 			_this.foundItems = result;
 		})
 		.catch(function(error){
@@ -38,19 +34,9 @@ function NarrowItDownAppController(MenuSearchService) { var _this = this;
 		})
 	}
 
-  _this.onRemove = function(index) {                                             /*DEBUG*/ console.log('removing:',index);
+  _this.onRemove = function(index) {                                             //*DEBUG*/ console.log('removing:',index);
     _this.foundItems.splice(index, 1);
   }
-
-	_this.isEmpty = function() {                                             
-    if ( (_this.foundItems != undefined) & (_this.foundItems.length == 0) )  {                      /*DEBUG*/ console.log('isEmpty');
-      return true;
-    }
-    else {                                                                    /*DEBUG*/ console.log('isNotEmpty');
-      return false;
-    }
-  }
-
 }
 
 MenuSearchService.$inject = ['$q', '$http'];
@@ -58,7 +44,7 @@ function MenuSearchService($q, $http) { var _this = this;
 
 	var itemsUrl = 'https://davids-restaurant.herokuapp.com/menu_items.json';
 
-	_this.getMatchedMenuItems = function(term) {                                  /*DEBUG*/ console.log('fetching:',itemsUrl);
+	_this.getMatchedMenuItems = function(term) {                                  //*DEBUG*/ console.log('fetching:',itemsUrl);
    	var deferred = $q.defer();
     var foundItems = [];
 
@@ -72,14 +58,14 @@ function MenuSearchService($q, $http) { var _this = this;
         method: "GET",
         url: itemsUrl,
       })
-      .then( function (response) {                                               /*DEBUG*/ console.log('got: ',response.data);
+      .then( function (response) {                                               //*DEBUG*/ console.log('got: ',response.data);
         var items = response.data.menu_items;
 
-        for (var i in items) {                                                   /*DEBUG*/ console.log('checking:',i);
+        for (var i in items) {                                                   //*DEBUG*/ console.log('checking:',i);
           var item = items[i];
 
-          if (item.description.search(term) > 0) {                               /*DEBUG*/ console.log('searching:',term);
-            foundItems.push(item);                                               /*DEBUG*/ console.log('pushing:',item);
+          if (item.description.search(term) > 0) {                               //*DEBUG*/ console.log('searching:',term);
+            foundItems.push(item);                                               //*DEBUG*/ console.log('pushing:',item);
           }
         }
         return foundItems;
